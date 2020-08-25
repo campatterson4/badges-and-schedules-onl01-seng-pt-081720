a7cc56970a1eb3d9878a5ad9c400ff05c84dea94
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-25 19:37:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(speakers)
   assign = []
-  speakers.each_with_index{speaker|index|
+  speakers.each_with_index{|speaker,index|
   assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
   }
   return assign
